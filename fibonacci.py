04f77a33c2a747fcd3cd13e685dbd8a6fa706bba
kushjaggi <67220319+kushjaggi@users.noreply.github.com>
2020-10-02 21:46:36 -0700

    Create fibonacci.py
    Dynamic Programming

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+n1, n2, n = map(int, input().split())
+sequence = [n1, n2]
+if n <= 2:
+    print(sequence[n-1])
+else:
+    for i in range(n-2):
+        sequence.append(sequence[-2] + sequence[-1]**2)
+    print(sequence[-1])
